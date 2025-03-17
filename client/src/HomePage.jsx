d05d80b3d913b75e26be663e07e152ceff7647a8
ethan <ethan.oke@ucalgary.ca>
2025-03-17 10:45:59 -0600

    Log in page styling finished

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './HomePage.css'
 
 function HomePage(props) {
   const [classroom, setClassroom] = useState([]);
--- client/src/LoginForm.jsx
+++ client/src/LoginForm.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import './LoginForm.css'
 
 function LoginForm(props) {
   return (
     <div>
       <h1>Fedora Learning</h1>
       <div className="container">
+        <h2>Log in to Fedora</h2>
         <form onSubmit={props.onSubmit}>
           <label htmlFor="email">Email: </label>
           <input type="text" id="email" name="email" required />
